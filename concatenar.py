663c9c48e802ef63a39fc4dc49535e551821809c
estrus28 <47963060@est.ort.edu.ar>
2024-08-15 21:43:34 -0300

    costo pero se termino

--- concatenar.py
+++ concatenar.py
@@ -1,6 +1,20 @@
 with open('Original.txt') as file: #abro el archivo original
-    for line in file:
-        content = line
-        with open('concatenado.txt', 'a') as file2: #abro el archivo de copia especificando que voy a escribir
-            file2.write(content) 
-    
+    lines1 = file.readlines()
+with open('concatenado.txt') as file2: #abro el archivo de copia especificando que voy a escribir
+    lines2 = file2.readlines()
+
+print(len(lines1))
+print(len(lines2))
+
+for i in range(0, len(lines2)):
+    if len(lines2) != len(lines1):
+        print("El segundo archivo debe tener menos lineas que el primero")
+        break
+    lines2[i] = lines2[i].strip() + lines1[i].strip()
+
+textoconcatenado = ""
+for line in lines2:
+    textoconcatenado += line + "\n"
+
+with open('concatenado.txt', 'w') as file3:
+    file3.write(textoconcatenado)
